4e051e9a3d9d0adbac0a42c49817a60842b991f8
Radoslav <rudix.lab@gmail.com>
2020-10-30 16:51:06 +0200

    c

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { FacebookProvider, LoginButton, Share } from "react-facebook";
 import { Helmet } from "react-helmet";
 import axios from "axios";
@@ -70,6 +70,20 @@ const App = ({ match }) => {
 
   return (
     <FacebookProvider appId="874162999784545">
+      <nav className="flex items-center justify-between flex-wrap bg-indigo-500 p-16">
+        <div className="flex items-center flex-shrink-0 text-white mr-6">
+          <svg
+            className="fill-current h-8 w-8 mr-2"
+            width="54"
+            height="54"
+            viewBox="0 0 54 54"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
+          </svg>
+          <span className="font-semibold text-xl tracking-tight">Късметче</span>
+        </div>
+      </nav>
       {match && match.params.id2 && (
         <Helmet>
           <meta
@@ -91,7 +105,7 @@ const App = ({ match }) => {
         {data.shid && (
           <div>
             <img
-              className="w-full"
+              className="w-full shadow-xl"
               src={`https://grafix.herokuapp.com/shot/${data.shid}.png`}
               alt="Sunset in the mountains"
             />
@@ -105,8 +119,8 @@ const App = ({ match }) => {
           </div>
         )}
         {match && match.params.id && !data.shid && (
-          <div className="flex justify-center">
-            <div className="max-w-sm rounded overflow-hidden shadow-lg">
+          <div className="flex justify-center -mt-16">
+            <div className="max-w-sm rounded overflow-hidden shadow-lg shadow-xl">
               <img
                 className="w-full"
                 src={Selected.img}
